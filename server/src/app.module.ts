02c380bdad1dece6bf51b4c5c86d8cf10e657852
owaisiqbal16 <m.owaisiqbal16@gmail.com>
2019-09-30 16:09:40 +0500

    Installed packages and made services for authentication

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,11 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/module/todos.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
-    TodosModule
+    TodosModule,
+    AuthModule,
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
