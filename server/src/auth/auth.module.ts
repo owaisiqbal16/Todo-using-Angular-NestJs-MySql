7ac77cccbaff6dca1e9f1eb0ad20959070454ba3
owaisiqbal16 <m.owaisiqbal16@gmail.com>
2019-10-01 15:11:02 +0500

    Refactored

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../users/user.entity';
+
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
@@ -9,14 +12,15 @@ import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
-    UsersModule,
-    PassportModule,
-    JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
-    }),
+    // UsersModule,
+    // PassportModule.register({ defaultStrategy: 'jwt'}),
+    // JwtModule.register({
+    //   secret: jwtConstants.secret,
+    //   signOptions: { expiresIn: '60s' },
+    // }),
+    TypeOrmModule.forFeature([User])
   ],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
-  exports: [AuthService]
+  // providers: [AuthService, LocalStrategy, JwtStrategy],
+  // exports: [AuthService]
 })
 export class AuthModule {}
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,3 +1,7 @@
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm'
+import { User } from './user.entity';
+
 import { Injectable } from '@nestjs/common';
 import { tsConstructorType } from '@babel/types';
 
@@ -5,30 +9,53 @@ export type User = any;
 
 @Injectable()
 export class UsersService {
-    private readonly users: User[];
+    // private readonly users: User[];
 
-    constructor(){
-        this.users = [
-            {
-                userId: 1,
-                username: 'john',
-                password: 'changeme',
-            },
-            {
-                userId: 2,
-                username: 'chris',
-                password: 'secret',
-            },
-            {
-                userId: 3,
-                username: 'maria',
-                password: 'guess',
-            },
-        ];
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
+    ){
+        // this.users = [
+        //     {
+        //         userId: 1,
+        //         username: 'john',
+        //         password: 'changeme',
+        //     },
+        //     {
+        //         userId: 2,
+        //         username: 'chris',
+        //         password: 'secret',
+        //     },
+        //     {
+        //         userId: 3,
+        //         username: 'maria',
+        //         password: 'guess',
+        //     },
+        // ];
     }
 
-    async findOne(username : string) : Promise<User | undefined> {
-        return this.users.find(user => user.username === username);
+    async findByEmail(email: string): Promise<User> {
+        return await this.userRepository.findOne({
+            where: {
+                email: email,
+            }
+        });
     }
+
+    async findById(id: number): Promise<User> {
+        return await this.userRepository.findOne({
+            where: {
+                id: id,
+            }
+        });
+    }
+
+    async create(user: User): Promise<User> {
+        return await this.userRepository.save(user);
+    }
+
+    // async findOne(username : string) : Promise<User | undefined> {
+    //     return this.users.find(user => user.username === username);
+    // }
 }
 
